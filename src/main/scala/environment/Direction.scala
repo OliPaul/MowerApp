b6e92cac15b7a234b4ade5268d7f315486db7d6d
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-01-04 01:44:38 +0100

    first commit

---
+++ src/main/scala/environment/Direction.scala
@@ -0,0 +1,60 @@
+package environment
+
+/** A trait used to represent the four cardinal directions: North, East, South and West
+ *
+ */
+sealed trait Direction {
+
+  /** Gets the next cardinal direction from the right
+   *
+   * @return   the next cardinal direction from the right
+   */
+  def nextFromRight() : Direction
+
+
+  /** Gets the next cardinal direction from the left
+   *
+   * @return   the next cardinal direction from the left
+   */
+  def nextFromLeft() : Direction
+}
+
+
+case object North extends Direction {
+  override def nextFromLeft(): Direction = West
+  override def nextFromRight(): Direction = East
+}
+
+case object East extends Direction {
+  override def nextFromLeft(): Direction = North
+  override def nextFromRight(): Direction = South
+}
+
+case object South extends Direction {
+  override def nextFromLeft(): Direction = East
+  override def nextFromRight(): Direction = West
+}
+
+case object West extends Direction {
+  override def nextFromLeft(): Direction = South
+  override def nextFromRight(): Direction = North
+}
+
+
+
+object Direction {
+
+  /** Gets the cardinal direction corresponding to a String.
+   *
+   * @param s    the string corresponding to a cardinal direction
+   * @return     An option containing the cardinal direction if the string corresponds to any of the direction
+   *             None otherwise
+   */
+  def apply(s: String): Option[Direction] = s match {
+    case "N" => Some(North);
+    case "E" => Some(East);
+    case "S" => Some(South);
+    case "W" => Some(West);
+    case _ => None
+  }
+}
---
+++ src/test/scala/environment/DirectionSpec.scala
@@ -0,0 +1,53 @@
+package environment
+
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+import org.scalatest.funsuite.AnyFunSuite
+
+class DirectionSpec extends AnyFunSuite {
+  test("Direction apply should return the correct Direction") {
+    assert(Direction("N") === Some(North))
+    assert(Direction("E") === Some(East))
+    assert(Direction("W") === Some(West))
+    assert(Direction("S") === Some(South))
+
+    val rightDirections = List("N", "E", "W", "S")
+    val wrongDirections = Gen.alphaStr suchThat (s => !rightDirections.contains(s))
+
+    forAll(wrongDirections) { x =>
+      Direction(x) === None
+    }
+  }
+
+  test("Direction nextFromLeft for North should return West") {
+    assert(North.nextFromLeft() === West)
+  }
+
+  test("Direction nextFromRight for North should return East") {
+    assert(North.nextFromRight() === East)
+  }
+
+  test("Direction nextFromLeft for East should return North") {
+    assert(East.nextFromLeft() === North)
+  }
+
+  test("Direction nextFromRight for East should return South") {
+    assert(East.nextFromRight() === South)
+  }
+
+  test("Direction nextFromLeft for South should return East") {
+    assert(South.nextFromLeft() === East)
+  }
+
+  test("Direction nextFromRight for South should return West") {
+    assert(South.nextFromRight() === West)
+  }
+
+  test("Direction nextFromLeft for West should return South") {
+    assert(West.nextFromLeft() === South)
+  }
+
+  test("Direction nextFromRight for West should return North") {
+    assert(West.nextFromRight() === North)
+  }
+}
