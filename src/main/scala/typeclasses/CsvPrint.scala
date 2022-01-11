3ef4a6f3163f4a13ed3166d5e1bfddde0ff75f6a
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-01-11 19:13:00 +0100

    Write test and implementation of CsvPrint

--- src/main/scala/typeclasses/CsvPrint.scala
+++ src/main/scala/typeclasses/CsvPrint.scala
@@ -1,81 +1,59 @@
 package typeclasses
 
-import environment.{Coordinate, Direction, East, Garden, Mower, North, South}
+import environment.{
+  Coordinate,
+  Direction,
+  Garden,
+  Mower,
+  WriteCsvCoordinates,
+  WriteCsvDirection,
+  WriteCsvMower
+}
 
-object Print {
+object CsvPrint {
 
-  /** A template trait typeclass used to get the string representation of objects.
-   *
-   */
   trait Show[T] {
-
-    /** Gets a string representation of the passed object.
-     *
-     * @param t      the object from which the string will be produced
-     * @return       a string representation of the object
-     */
     def show(t: T): String
   }
 
   object Show {
 
-    /** Implicit evidence of the implementation of the Show typeclass for Coordinate class
-     *
-     */
     implicit val CoordinateShow = new Show[Coordinate] {
       def show(
           c: Coordinate
-      ): String = c.x.toString + " " + c.y.toString
+      ): String = WriteCsvCoordinates.writes(c)
     }
 
-    /** Implicit evidence of the implementation of the Show typeclass for Direction class
-     *
-     */
     implicit val DirectionShow = new Show[Direction] {
       def show(
           d: Direction
-      ): String = d match {
-        case North => "N"
-        case East  => "E"
-        case South => "S"
-        case _     => "W"
-      }
+      ): String = WriteCsvDirection.writes(d)
     }
 
-    /** Implicit evidence of the implementation of the Show typeclass for Mower class
-     *
-     */
     implicit val MowerShow = new Show[Mower] {
       def show(
           mower: Mower
-      ): String =
-        Print.print(mower.position) + " - " + Print.print(mower.direction)
+      ): String = WriteCsvMower.writes(mower)
     }
 
-    /** Implicit evidence of the implementation of the Show typeclass for Garden class
-     *
-     */
     implicit val GardenShow = new Show[Garden] {
       def show(
           garden: Garden
       ): String = {
-        val firstLine = Print.print(garden.topRightPosition) + "\n"
+        val header =
+          "numéro;début_x;début_y;début_direction;fin_x;fin_y;fin_direction;instructions\n"
         val mowersStr =
-          garden.mowers.reverse
-            .map(mower => Print.print(mower))
+          garden.mowers.reverse.zipWithIndex
+            .map {
+              case (mower, i) =>
+                (i + 1).toString + ";" + CsvPrint.print(mower)
+            }
             .mkString("\n")
-        firstLine + mowersStr
+        header + mowersStr
       }
     }
   }
 
-  /** Gets the string representation of an object
-   *
-   * @param t      the object from which the string will be produced
-   * @param ev     the implicit evidence that the object implements the Show typeclass
-   * @tparam T     template parameter
-   * @return       the string representation of the object
-   */
   def print[T](t: T)(
       implicit ev: Show[T]
   ): String = ev.show(t)
--- src/test/scala/typeclasses/CsvPrintSpec.scala
+++ src/test/scala/typeclasses/CsvPrintSpec.scala
@@ -1,53 +1,17 @@
 package typeclasses
 
-import environment.{Coordinate, East, Garden, Mower, North, South, West}
-//import org.scalacheck.Prop.forAll
+import environment.{Coordinate, Garden, Mower, North, South}
 import org.scalatest.funsuite.AnyFunSuite
 
-class PrintSpec extends AnyFunSuite {
+class CsvPrintSpec extends AnyFunSuite {
 
-  /*test("Print print Coordinate should print the coordinates") {
-    forAll { (n: Int, m: Int) =>
-      Print.print(Coordinate(n, m)) === n + " " + m
-    }
-  }*/
-
-  /*test("Print print Direction should print the correct direction") {
-    assert(Print.print(North.asInstanceOf[Direction]) === "North")
-    assert(Print.print(East.asInstanceOf[Direction]) === "East")
-    assert(Print.print(South.asInstanceOf[Direction]) === "South")
-    assert(Print.print(West.asInstanceOf[Direction]) === "West")
-  }*/
-
-  /*test("Print print Mower should print the mower") {
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), North)
-      Print.print(mower) === n + " " + m + " - " + "North"
-    }
-
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), East)
-      Print.print(mower) === n + " " + m + " - " + "East"
-    }
-
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), South)
-      Print.print(mower) === n + " " + m + " - " + "South"
-    }
-
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), West)
-      Print.print(mower) === n + " " + m + " - " + "West"
-    }
-  }*/
-
-  test("Print print Garden should print the garden with all its mowers") {
+  test("Print Garden should print the garden with all its mowers in csv format") {
     val garden = Garden(Coordinate(10, 10), List())
 
     val m1 = Mower(
-      Coordinate(0, 0),
+      Coordinate(9, 9),
       North,
-      Coordinate(0, 0),
+      Coordinate(9, 9),
       North,
       List("D", "A", "A", "F", "X", "G", "A", "A", "D")
     )
@@ -58,32 +22,14 @@ class PrintSpec extends AnyFunSuite {
       South,
       List("D", "A", "A", "F", "X", "G", "A", "A", "D")
     )
-    val m3 = Mower(
-      Coordinate(2, 8),
-      West,
-      Coordinate(2, 8),
-      West,
-      List("D", "A", "A", "F", "X", "G", "A", "A", "D")
-    )
-    val m4 = Mower(
-      Coordinate(9, 9),
-      East,
-      Coordinate(9, 9),
-      East,
-      List("D", "A", "A", "F", "X", "G", "A", "A", "D")
-    )
-    //val m5 = Mower(Coordinate(42, 42), West)
 
     val finalGarden = garden
       .addMower(Right(m1))
       .addMower(Right(m2))
-      .addMower(Left("AAAAAA"))
-      .addMower(Right(m3))
-      .addMower(Right(m4))
 
-    val expectedStr = "10 10\n" + Print.print(m1) + "\n" + Print.print(m2) + "\n" + Print
-      .print(m3) + "\n" + Print.print(m4)
-    assert(Print.print(finalGarden) === expectedStr)
+    val expectedStr = "numéro;début_x;début_y;début_direction;fin_x;fin_y;fin_direction;instructions\n1;" + CsvPrint
+      .print(m1) + "\n2;" + CsvPrint.print(m2)
+    assert(CsvPrint.print(finalGarden) === expectedStr)
   }
 
 }
