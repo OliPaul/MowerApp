f45cf0826c2f09bc4536e6848ed8ef18f87b876e
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-01-11 19:12:39 +0100

    Write test and implementation of JsonPrint

--- src/main/scala/typeclasses/JsonPrint.scala
+++ src/main/scala/typeclasses/JsonPrint.scala
@@ -1,5 +1,52 @@
 package typeclasses
 
-object JsonPrint {
+import environment.{
+  Coordinate,
+  Direction,
+  Garden,
+  Mower,
+  WriteGarden,
+  WriteJsonCoordinates,
+  WriteJsonDirection,
+  WriteJsonMower
+}
+import play.api.libs.json.JsValue
 
+object JsonPrint {
+  trait Show[T] {
+    def show(t: T): JsValue
+  }
+
+  object Show {
+
+    implicit val CoordinateShow = new Show[Coordinate] {
+      def show(
+          c: Coordinate
+      ): JsValue = WriteJsonCoordinates.writes(c)
+    }
+
+    implicit val DirectionShow = new Show[Direction] {
+      def show(
+          d: Direction
+      ): JsValue = WriteJsonDirection.writes(d)
+    }
+
+    implicit val MowerShow = new Show[Mower] {
+      def show(
+          mower: Mower
+      ): JsValue = WriteJsonMower.writes(mower)
+    }
+
+    implicit val GardenShow = new Show[Garden] {
+      def show(
+          garden: Garden
+      ): JsValue = WriteGarden.writes(garden)
+    }
+  }
+
+  def print[T](
+      t: T
+  )(
+      implicit ev: Show[T]
+  ): JsValue = ev.show(t)
 }
--- src/test/scala/typeclasses/JsonPrintSpec.scala
+++ src/test/scala/typeclasses/JsonPrintSpec.scala
@@ -1,89 +1,89 @@
 package typeclasses
 
-import environment.{Coordinate, East, Garden, Mower, North, South, West}
-//import org.scalacheck.Prop.forAll
+import environment._
+import play.api.libs.json.Json.toJson
 import org.scalatest.funsuite.AnyFunSuite
+import play.api.libs.json.Json
 
-class CsvPrintSpec extends AnyFunSuite {
+@SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
+class JsonPrintSpec extends AnyFunSuite {
 
-  /*test("Print print Coordinate should print the coordinates") {
-    forAll { (n: Int, m: Int) =>
-      Print.print(Coordinate(n, m)) === n + " " + m
-    }
-  }*/
-
-  /*test("Print print Direction should print the correct direction") {
-    assert(Print.print(North.asInstanceOf[Direction]) === "North")
-    assert(Print.print(East.asInstanceOf[Direction]) === "East")
-    assert(Print.print(South.asInstanceOf[Direction]) === "South")
-    assert(Print.print(West.asInstanceOf[Direction]) === "West")
-  }*/
-
-  /*test("Print print Mower should print the mower") {
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), North)
-      Print.print(mower) === n + " " + m + " - " + "North"
-    }
-
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), East)
-      Print.print(mower) === n + " " + m + " - " + "East"
-    }
-
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), South)
-      Print.print(mower) === n + " " + m + " - " + "South"
-    }
-
-    forAll { (n : Int, m : Int) =>
-      val mower = Mower(Coordinate(n, m), West)
-      Print.print(mower) === n + " " + m + " - " + "West"
-    }
-  }*/
-
-  test("Print print Garden should print the garden with all its mowers") {
+  test("Print Garden should print the garden with all its mowers in json") {
     val garden = Garden(Coordinate(10, 10), List())
 
     val m1 = Mower(
-      Coordinate(0, 0),
+      Coordinate(1, 2),
       North,
-      Coordinate(0, 0),
+      Coordinate(1, 2),
       North,
-      List("D", "A", "A", "F", "X", "G", "A", "A", "D")
+      List("G", "A", "G", "A", "G", "A", "G", "A", "A")
     )
     val m2 = Mower(
-      Coordinate(5, 0),
-      South,
-      Coordinate(5, 0),
-      South,
-      List("D", "A", "A", "F", "X", "G", "A", "A", "D")
-    )
-    val m3 = Mower(
-      Coordinate(2, 8),
-      West,
-      Coordinate(2, 8),
-      West,
-      List("D", "A", "A", "F", "X", "G", "A", "A", "D")
-    )
-    val m4 = Mower(
-      Coordinate(9, 9),
+      Coordinate(3, 3),
       East,
-      Coordinate(9, 9),
+      Coordinate(3, 3),
       East,
-      List("D", "A", "A", "F", "X", "G", "A", "A", "D")
+      List("A", "A", "D", "A", "A", "D", "A", "D", "D", "A")
     )
-    //val m5 = Mower(Coordinate(42, 42), West)
 
     val finalGarden = garden
       .addMower(Right(m1))
       .addMower(Right(m2))
-      .addMower(Left("AAAAAA"))
-      .addMower(Right(m3))
-      .addMower(Right(m4))
 
-    val expectedStr = "numéro;début_x;début_y;début_direction;fin_x;fin_y;fin_direction;instructions\n1;" + CsvPrint.print(m1) + "\n2;" + CsvPrint.print(m2) + "\n3;" + CsvPrint
-      .print(m3) + "\n4;" + CsvPrint.print(m4)
-    assert(CsvPrint.print(finalGarden) === expectedStr)
+    val expectedStr = Json.toJson(
+      Map(
+        "limite" -> WriteJsonCoordinates.writes(Coordinate(10, 10)),
+        "tondeuses" -> toJson(
+          List(
+            toJson(
+              Map(
+                "debut" -> toJson(
+                  Map(
+                    "point"     -> WriteJsonCoordinates.writes(Coordinate(1, 2)),
+                    "direction" -> WriteJsonDirection.writes(Direction("N").get)
+                  )
+                ),
+                "instructions" -> toJson(
+                  List("G", "A", "G", "A", "G", "A", "G", "A", "A")
+                ),
+                "fin" -> toJson(
+                  Map(
+                    "point" -> WriteJsonCoordinates.writes(Coordinate(1, 2)),
+                    "direction" -> WriteJsonDirection.writes(
+                      Direction("N").get
+                    )
+                  )
+                )
+              )
+            ),
+            toJson(
+              Map(
+                "debut" -> toJson(
+                  Map(
+                    "point" -> WriteJsonCoordinates.writes(Coordinate(3, 3)),
+                    "direction" -> WriteJsonDirection.writes(
+                      Direction("E").get
+                    )
+                  )
+                ),
+                "instructions" -> toJson(
+                  List("A", "A", "D", "A", "A", "D", "A", "D", "D", "A")
+                ),
+                "fin" -> toJson(
+                  Map(
+                    "point" -> WriteJsonCoordinates.writes(Coordinate(3, 3)),
+                    "direction" -> WriteJsonDirection.writes(
+                      Direction("E").get
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+    assert(JsonPrint.print(finalGarden) === expectedStr)
   }
 
 }
